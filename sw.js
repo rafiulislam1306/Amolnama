3aaf15079c754e4d8beca4af235155ac278c8bac
dmericcoleman <dmericcoleman@gmail.com>
2026-04-23 13:04:47 +0600

    upload to github

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'ledger-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Return cached version or fetch from internet
+        return response || fetch(event.request);
+      })
+  );
+});
