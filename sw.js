8a573392392903ef2dc26e7404fffb7c55b3758f
Rafiul Islam <rafiulislam1306@gmail.com>
2026-04-23 16:32:48 +0600

    error fix

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
-const CACHE_NAME = 'amolnama-v5';
+const CACHE_NAME = 'amolnama-v6';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -16,11 +16,25 @@ self.addEventListener('install', event => {
   );
 });
 
+// The cleanup crew! This deletes the broken v5 cache.
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Return cached version or fetch from internet
         return response || fetch(event.request);
       })
   );
